f944bb2f4e14cc3d7223e1275a273b9ae6aff059
Berkan Buyuk <berkanbyk55@gmail.com>
2023-01-03 23:26:52 +0300

    Navbar created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      Berkan
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="videos/:id" element={<Detail />} />
+          <Route path="search/: keyword" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
